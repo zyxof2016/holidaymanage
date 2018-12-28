6f88cea0551b0c2413546b3d657711074f15f672
zyx <1769886476@qq.com>
2018-12-28 14:58:21 +0800

    初始化项目

---
+++ src/main/java/com/zyx/holidaymanage/HolidaymanageApplication.java
@@ -0,0 +1,14 @@
+package com.zyx.holidaymanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaymanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidaymanageApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/zyx/holidaymanage/HolidaymanageApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zyx.holidaymanage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HolidaymanageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
